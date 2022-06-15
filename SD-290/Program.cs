f0305f9b89b6a080058d50bd1246f56d0c5df257
Jan <jancecinggao@gmail.com>
2022-06-15 18:36:59 -0500

    initial commit

--- SD-290/Program.cs
+++ SD-290/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lab 4
+
+List<int> maxNumList = new List<int> { 1, 2, 3 };
+
+int GetHighestValueInList(List<int> intList)
+{
+    return GetHighestValueInList(new List<int>());
+}
+
+List<int> MaxNumbersInLists(List<List<int>> intList)
+{
+    List<int> maxNumbers = new List<int>();
+    return maxNumbers;
+}
+
+string HighestGrade(List<List<int>> gradeList)
+{
+    string grade = "";
+    return grade;
+}
+
+List<int> OrderByLooping(List<int> listToOrder)
+{
+    List<int> orderedList = new List<int>();
+    return orderedList;
+}
