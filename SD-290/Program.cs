7b5fdc78a0ba4c2be141a49cd2326d5af9520f01
Jan <jancecinggao@gmail.com>
2022-06-15 22:51:42 -0500

    explained why there can be only one famous person at most in any 2D array, or atleast tried to

--- SD-290/Program.cs
+++ SD-290/Program.cs
@@ -1,1 +1,27 @@
 ﻿// Lab 6
+
+/*
+ * A boolean 2D array represents the relationships between guests at a party
+ * The goal of the algorithm is to find the famous person
+ * This person doesn’t know anyone else at the party, but everyone else knows of them.
+ * 
+ * explain why there can be only one famous person at most in any 2D array:
+ * 
+ * if there are two famous people neither one of them fit the line "but everyone else knows of them"
+ *                  or
+ * two famous people will result in one row/column of the array to have an outstanding false,
+ * making it hard to find the famous person
+ */
+
+int FindFamous(int[,] room)
+{
+
+    /*
+    if(famous = true)
+    {
+        return famous[index];
+    }
+    */
+
+    return -1;
+}
